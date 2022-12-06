c5adaca1438d737b3fe3f7ca416d5143f3d8ce2d
mekapeu <kahtchekmekapeu@gmail.com>
2022-12-05 20:53:22 -0800

    function of your command interpreter

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,26 +1,6 @@
-#!/usr/bin/python3
-# Fabric script that generates a .tgz archive from
-# the contents of the web_static folder of your AirBnB Clone
-# repo, using the function do_pack
-"""Fabric file"""
-from fabric.api import *
-import datetime
-
-
-def do_pack():
-    """Do_pack function"""
-    today = datetime.datetime.now()
-    file_local = 'versions/web_static_{}{}{}{}{}{}.tgz'.format(today.year,
-                                                               today.month,
-                                                               today.day,
-                                                               today.hour,
-                                                               today.minute,
-                                                               today.second)
-
-    local('mkdir -p versions')
-    check = local('tar -cvzf {} web_static'.format(file_local))
-    if check.failed:
-        return None
-    else:
-        return file_local
-	
+-- MySQL setup development
+-- Script for create database and user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
