8fa595e384ad34f078349c29e1ada3de35e6d1e1
mekapeu <kahtchekmekapeu@gmail.com>
2022-12-22 19:05:43 -0800

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,13 @@
--- MySQL setup development
--- Script for create database and user
+-- write a script that prepares a MySQL server for the project
+-- create database with the right stuff
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- create user if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost';
+-- set password for user
+SET PASSWORD FOR 'hbnb_dev'@'localhost' = 'hbnb_dev_pwd';
+-- grant privileges to user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+-- grant select privileges to user on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,13 @@
--- MySQL setup development
--- Script for create database and user in testDatabase
+-- write a script that prepares a MySQL server for the project
+-- create test database with the right stuff
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- create user if doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+-- set password for user
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+-- grant privileges to user
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- grant select privileges to user on performance_schema database
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
