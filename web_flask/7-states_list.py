f5a22b51e3dfddbf55c6dd8e84571c8b778ee232
mekapeu <kahtchekmekapeu@gmail.com>
2023-01-15 16:28:17 -0800

    ou must use storage for fetching data from

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""simple flask app
+"""
+from flask import Flask, render_template
+from models import storage
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """list states sorted by name
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
+    
